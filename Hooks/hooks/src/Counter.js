058e7d59eb0d1318195aeb698a75ece26d8574c1
Vysakhdn <vysakhdn123@gmail.com>
2023-09-25 10:39:57 +0530

    handsOn

---
+++ Hooks/hooks/src/Counter.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function CounterApp() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default CounterApp;
